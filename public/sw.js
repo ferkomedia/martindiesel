8af256b877a94cc70e49f710b2a9f3cab16b715e
ferkomedia <info@ferkomedia.sk>
2026-03-22 09:32:23 +0100

    Fix service worker caching

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,10 @@
-const CACHE = 'md-v1';
-const PRECACHE = ['/', '/produkty', '/blog', '/kontakt', '/o-nas'];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)).then(() => self.skipWaiting()));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
-});
-
-self.addEventListener('fetch', (e) => {
-    if (e.request.method !== 'GET') return;
-    e.respondWith(
-        caches.match(e.request).then((cached) => {
-            const fetched = fetch(e.request).then((res) => {
-                if (res.ok) {
-                    const clone = res.clone();
-                    caches.open(CACHE).then((c) => c.put(e.request, clone));
-                }
-                return res;
-            }).catch(() => cached);
-            return cached || fetched;
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
